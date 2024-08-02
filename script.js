ab729050b6f8f4388fcf277a56d739644f2c9b83
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-08-01 21:55:23 -0300

    feat: attempt function playRound

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     const options = ["rock", "paper", "scissor"];
     const random = Math.floor(Math.random() * options.length)
@@ -6,7 +9,27 @@ function getComputerChoice() {
 
 function getHumanChoice(){
     const humanOption = prompt("Choose your option: Rock, Paper or Scissor")
-    return humanOption;
+    return humanOption.toLowerCase();
 }
-// getComputerChoice()
-// getHumanChoice()
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log("It's a tie");
+    }else{
+        if (humanChoice === "rock" && computerChoice === "scissor" 
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissor" && computerChoice === "paper") {
+            console.log("You win!");
+            humanScore++;
+        }else{
+            console.log("The computer wins!");
+            computerScore++
+        }
+    }
+}
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+
+playRound(getHumanChoice(), getComputerChoice())
